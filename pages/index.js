e56531bf3b8670553a4a4fd5f3258de576360eff
Ivandro Panzo <ivandropanzo583@gmail.com>
2024-09-20 11:36:11 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-green-950 w-screen h-98">
+      <h1 className="text-3xl font-bold underline text-white">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
